b3135a921e888d73bfd7b7cf4188a0ac33808902
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2025-06-17 23:40:51 +0300

    updates

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mhamedelbaz/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mhamedelbaz/InteligenceSolutions/zurex_mobile_flutter_admin"
+export "FLUTTER_ROOT=/Users/innovadigits/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/innovadigits/StudioProjects/zurex_mobile_flutter_admin"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
